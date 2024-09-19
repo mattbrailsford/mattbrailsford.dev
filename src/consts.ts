99dcbc0a529557bf4de519c6023ec7b3c1f7763d
mattbrailsford <me@mattbrailsford.com>
2024-09-19 14:27:03 +0100

    use domain environment variable to site name

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 ﻿// Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'mattbrailsford.dev';
+export const SITE_TITLE = `${import.meta.env.DOMAIN}`;
 export const SITE_DESCRIPTION = 'Welcome to my personal blog. I mostly write about umbraco.';
 
 export const SOCIAL_LINKS = [
