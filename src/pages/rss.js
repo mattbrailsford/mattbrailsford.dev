6d94eba11ce65f27711ed50b1a05184cf94cb7a8
mattbrailsford <me@mattbrailsford.com>
2024-09-19 10:19:12 +0100

    Setup rss

---
+++ src/pages/rss.js
@@ -0,0 +1,19 @@
+﻿import rss from '@astrojs/rss';
+import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import {getCollection} from "astro:content";
+
+export async function GET(context) {
+    const posts = await getCollection('blogPosts');
+    return rss({
+        title: SITE_TITLE,
+        description: SITE_DESCRIPTION,
+        site: context.site ?? "https://mattbrailsford.dev",
+        trailingSlash: false,
+        items: posts.map((post) => ({
+            title: post.data.title,
+            description: post.data.description,
+            pubDate: post.data.date,
+            link: `/${post.data.slug}`,
+        })),
+    })
+}
