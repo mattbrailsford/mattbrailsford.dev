43a7c59da8bbdd8fec0f38a9d97b77d93b74bbd6
mattbrailsford <me@mattbrailsford.com>
2024-09-21 14:49:26 +0100

    Styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+import TailwindTypography from '@tailwindcss/typography';
+
+const plugin = require('tailwindcss/plugin');
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['Red Hat Display'],
+				display: ['Source Sans 3'],
+			}
+		},
 	},
-	plugins: [],
+	plugins: [
+		TailwindTypography,
+		plugin(function ({addVariant}) {
+			addVariant('prose-inline-code', '&.prose :where(:not(pre)>code):not(:where([class~="not-prose"] *))');
+		})
+	],
 }
