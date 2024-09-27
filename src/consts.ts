10163b6372a833e94c6e507e6197fd90b5c4879d
mattbrailsford <me@mattbrailsford.com>
2024-09-27 14:41:58 +0100

    Update default description

--- src/consts.ts
+++ src/consts.ts
@@ -2,25 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = `${import.meta.env.DOMAIN}`;
-export const SITE_DESCRIPTION = 'Welcome to my personal blog. I mostly write about umbraco.';
-
-export const SOCIAL_LINKS = [
-    {
-        url: 'https://github.com/reegodev',
-        icon: '/github-dark.svg',
-        label: 'github.com/reegodev',
-        name: 'Github profile',
-    },
-    {
-        url: 'https://twitter.com/reegodev',
-        icon: '/twitter.svg',
-        label: 'twitter.com/reegodev',
-        name: 'Twitter profile',
-    },
-    {
-        url: 'https://www.polywork.com/reegodev',
-        icon: '/polywork.svg',
-        label: 'polywork.com/reegodev',
-        name: 'Polywork profile',
-    },
-] as const
+export const SITE_DESCRIPTION = 'Welcome to my personal blog where I mostly write about Umbraco.';
